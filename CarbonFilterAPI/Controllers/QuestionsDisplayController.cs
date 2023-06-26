1c2404f512ba35c86e6d1cba4e21f2fc527b6f55
rohithpill <94391093+rohithpill@users.noreply.github.com>
2023-06-26 09:57:05 +0530

    working api

--- CarbonFilterAPI/Controllers/QuestionsDisplayController.cs
+++ CarbonFilterAPI/Controllers/QuestionsDisplayController.cs
@@ -22,40 +22,39 @@ namespace CarbonFilterAPI.Controllers
 
         // GET: api/<ValuesController>
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<QuestionDisplay>>> GetQuestions()
+        public Task<ActionResult<IEnumerable<QuestionDisplay>>> GetQuestions()
         {
             if (_context.Questions == null)
             {
-                return NotFound();
+                return Task.FromResult<ActionResult<IEnumerable<QuestionDisplay>>>(NotFound());
             }
 
             List<QuestionDisplay> questionDisplayList = new List<QuestionDisplay>();
 
             foreach (var question in _context.Questions) 
             {
-                var category = _context.Categories.Find(question.CategoryId);
-                var dropDown = _context.DropDowns.Find(question.DropDownId);
-                FormattableString getPickListItems = $"SELECT * FROM PickListItems WHERE QuestionId={question.QuestionId}";
-                FormattableString getDropDownOptions = $"SELECT * FROM DropDownOptions WHERE DropDownId={question.DropDownId}";
+                var category = _context.Categories.Single(item => item.CategoryId == question.CategoryId);
+                var dropDown = _context.DropDowns.Single(item => item.DropDownId == question.DropDownId);
+                var pickListItems = _context.PickListItems.Where(item => item.QuestionId == question.QuestionId);
+                var dropDownOptions = _context.DropDownOptions.Where(item => item.DropDownId == question.DropDownId);
 
-                var pickListItems = _context.PickListItems.FromSql(getPickListItems).ToList();
-                var dropDownOptions = _context.DropDownOptions.FromSql(getDropDownOptions).ToList();
-
-                QuestionDisplay questionDisplay = new QuestionDisplay();
-                questionDisplay.CategoryName = (category is null) ? string.Empty : category.CategoryName;
-                questionDisplay.QuestionNum = question.QuestionNum;
-                questionDisplay.QuestionName = question.QuestionName;
-                questionDisplay.InfoNotes = question.InfoNotes;
-                questionDisplay.kgCo2eEmissions = question.kgCo2eEmissions;
-                questionDisplay.PickListItems = (pickListItems.Count > 0)
-                    ? pickListItems.ToDictionary(x => x.PickListItemId, x => x.PickListItemName ?? string.Empty) : null;
-                questionDisplay.DropDownOptions = (dropDownOptions.Count > 0)
-                    ? dropDownOptions.ToDictionary(x => x.DropDownOptionId, x => x.Range) : null;
+                QuestionDisplay questionDisplay = new QuestionDisplay
+                {
+                    CategoryName = (category is null) ? string.Empty : category.CategoryName,
+                    QuestionNum = question.QuestionNum,
+                    QuestionName = question.QuestionName,
+                    InfoNotes = question.InfoNotes,
+                    kgCo2eEmissions = question.kgCo2eEmissions,
+                    PickListItems = (pickListItems.Count() > 0)
+                    ? pickListItems.ToDictionary(x => x.PickListItemId, x => x.PickListItemName ?? string.Empty) : null,
+                    DropDownOptions = (dropDownOptions.Count() > 0)
+                    ? dropDownOptions.ToDictionary(x => x.DropDownOptionId, x => x.Range) : null
+                };
 
                 questionDisplayList.Add(questionDisplay);
             }
 
-            return questionDisplayList;
+            return Task.FromResult<ActionResult<IEnumerable<QuestionDisplay>>>(questionDisplayList);
         }
 
         // GET api/<ValuesController>/5
@@ -76,22 +75,25 @@ namespace CarbonFilterAPI.Controllers
 
             var category = _context.Categories.Find(question.CategoryId);
             var dropDown = _context.DropDowns.Find(question.DropDownId);
-            FormattableString getPickListItems = $"SELECT * FROM PickListItems WHERE QuestionId={question.QuestionId}";
-            FormattableString getDropDownOptions = $"SELECT * FROM DropDownOptions WHERE DropDownId={question.DropDownId}";
+            //FormattableString getPickListItems = $"SELECT * FROM PickListItems WHERE QuestionId={question.QuestionId}";
+            //FormattableString getDropDownOptions = $"SELECT * FROM DropDownOptions WHERE DropDownId={question.DropDownId}";
+            //var pickListItems = _context.PickListItems.FromSql(getPickListItems).ToList();
+            //var dropDownOptions = _context.DropDownOptions.FromSql(getDropDownOptions).ToList();
+            var pickListItems = _context.PickListItems.Where(item => item.QuestionId == question.QuestionId);
+            var dropDownOptions = _context.DropDownOptions.Where(item => item.DropDownId == question.DropDownId);
 
-            var pickListItems = _context.PickListItems.FromSql(getPickListItems).ToList();
-            var dropDownOptions = _context.DropDownOptions.FromSql(getDropDownOptions).ToList();
-
-            QuestionDisplay questionDisplay = new QuestionDisplay();
-            questionDisplay.CategoryName = (category is null) ? null : category.CategoryName;
-            questionDisplay.QuestionNum = question.QuestionNum;
-            questionDisplay.QuestionName = question.QuestionName;
-            questionDisplay.InfoNotes = question.InfoNotes;
-            questionDisplay.kgCo2eEmissions = question.kgCo2eEmissions;
-            questionDisplay.PickListItems = (pickListItems.Count > 0) 
-                ? pickListItems.ToDictionary(x => x.PickListItemId, x => x.PickListItemName ?? string.Empty) : null;
-            questionDisplay.DropDownOptions = (dropDownOptions.Count > 0) 
-                ? dropDownOptions.ToDictionary(x => x.DropDownOptionId, x => x.Range) : null;
+            QuestionDisplay questionDisplay = new QuestionDisplay
+            {
+                CategoryName = (category is null) ? null : category.CategoryName,
+                QuestionNum = question.QuestionNum,
+                QuestionName = question.QuestionName,
+                InfoNotes = question.InfoNotes,
+                kgCo2eEmissions = question.kgCo2eEmissions,
+                PickListItems = (pickListItems.Count() > 0)
+                ? pickListItems.ToDictionary(x => x.PickListItemId, x => x.PickListItemName ?? string.Empty) : null,
+                DropDownOptions = (dropDownOptions.Count() > 0)
+                ? dropDownOptions.ToDictionary(x => x.DropDownOptionId, x => x.Range) : null
+            };
 
             return questionDisplay;
         }
